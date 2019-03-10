67447ce13b906688940f139189c71230b3f738b5
Krzysztof <k.niedzwiecki@hotmail.com>
2019-03-11 00:18:17 +0100

    time, echo, public orders

--- Comp02_NetAssembly/Form1.Designer.cs
+++ Comp02_NetAssembly/Form1.Designer.cs
@@ -31,10 +31,10 @@
             this.tbStockInput = new System.Windows.Forms.TextBox();
             this.btnStockQuoteSync = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btnStockQuoteAsync = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.tbStockOutput = new System.Windows.Forms.TextBox();
+            this.btnStockQuoteAsync = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
             this.tbIpGeoOutput = new System.Windows.Forms.TextBox();
@@ -42,8 +42,34 @@
             this.label4 = new System.Windows.Forms.Label();
             this.btnIpgeoSync = new System.Windows.Forms.Button();
             this.tbIpGeoInput = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbTimeOutput = new System.Windows.Forms.TextBox();
+            this.btnTimeAsync = new System.Windows.Forms.Button();
+            this.btnTimeSync = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbEchoOutput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnEchoSync = new System.Windows.Forms.Button();
+            this.tbEchoInput = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbOrderOutput = new System.Windows.Forms.TextBox();
+            this.btnOrderAsync = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnOrderSync = new System.Windows.Forms.Button();
+            this.tbOrderer = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tbOrder = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tbVoivodeship = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
             this.SuspendLayout();
             // 
             // tbStockInput
@@ -79,25 +105,6 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Stock Quote";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Input:";
-            // 
-            // btnStockQuoteAsync
-            // 
-            this.btnStockQuoteAsync.Location = new System.Drawing.Point(122, 45);
-            this.btnStockQuoteAsync.Name = "btnStockQuoteAsync";
-            this.btnStockQuoteAsync.Size = new System.Drawing.Size(50, 23);
-            this.btnStockQuoteAsync.TabIndex = 2;
-            this.btnStockQuoteAsync.Text = "Async";
-            this.btnStockQuoteAsync.UseVisualStyleBackColor = true;
-            this.btnStockQuoteAsync.Click += new System.EventHandler(this.btnStockQuoteAsync_Click);
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
@@ -113,6 +120,25 @@
             this.tbStockOutput.Name = "tbStockOutput";
             this.tbStockOutput.Size = new System.Drawing.Size(113, 20);
             this.tbStockOutput.TabIndex = 3;
+            // 
+            // btnStockQuoteAsync
+            // 
+            this.btnStockQuoteAsync.Location = new System.Drawing.Point(122, 45);
+            this.btnStockQuoteAsync.Name = "btnStockQuoteAsync";
+            this.btnStockQuoteAsync.Size = new System.Drawing.Size(50, 23);
+            this.btnStockQuoteAsync.TabIndex = 2;
+            this.btnStockQuoteAsync.Text = "Async";
+            this.btnStockQuoteAsync.UseVisualStyleBackColor = true;
+            this.btnStockQuoteAsync.Click += new System.EventHandler(this.btnStockQuoteAsync_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input:";
             // 
             // groupBox2
             // 
@@ -183,11 +209,239 @@
             this.tbIpGeoInput.TabIndex = 0;
             this.tbIpGeoInput.Text = "213.180.141.140";
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.tbTimeOutput);
+            this.groupBox3.Controls.Add(this.btnTimeAsync);
+            this.groupBox3.Controls.Add(this.btnTimeSync);
+            this.groupBox3.Location = new System.Drawing.Point(12, 317);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(187, 90);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Time";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Output:";
+            // 
+            // tbTimeOutput
+            // 
+            this.tbTimeOutput.Location = new System.Drawing.Point(59, 48);
+            this.tbTimeOutput.Name = "tbTimeOutput";
+            this.tbTimeOutput.Size = new System.Drawing.Size(113, 20);
+            this.tbTimeOutput.TabIndex = 3;
+            // 
+            // btnTimeAsync
+            // 
+            this.btnTimeAsync.Location = new System.Drawing.Point(123, 19);
+            this.btnTimeAsync.Name = "btnTimeAsync";
+            this.btnTimeAsync.Size = new System.Drawing.Size(50, 23);
+            this.btnTimeAsync.TabIndex = 2;
+            this.btnTimeAsync.Text = "Async";
+            this.btnTimeAsync.UseVisualStyleBackColor = true;
+            this.btnTimeAsync.Click += new System.EventHandler(this.btnTimeAsync_Click);
+            // 
+            // btnTimeSync
+            // 
+            this.btnTimeSync.Location = new System.Drawing.Point(60, 19);
+            this.btnTimeSync.Name = "btnTimeSync";
+            this.btnTimeSync.Size = new System.Drawing.Size(50, 23);
+            this.btnTimeSync.TabIndex = 1;
+            this.btnTimeSync.Text = "Sync";
+            this.btnTimeSync.UseVisualStyleBackColor = true;
+            this.btnTimeSync.Click += new System.EventHandler(this.btnTimeSync_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Controls.Add(this.tbEchoOutput);
+            this.groupBox4.Controls.Add(this.button1);
+            this.groupBox4.Controls.Add(this.label7);
+            this.groupBox4.Controls.Add(this.btnEchoSync);
+            this.groupBox4.Controls.Add(this.tbEchoInput);
+            this.groupBox4.Location = new System.Drawing.Point(205, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(187, 195);
+            this.groupBox4.TabIndex = 6;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Echo";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Output:";
+            // 
+            // tbEchoOutput
+            // 
+            this.tbEchoOutput.Location = new System.Drawing.Point(59, 74);
+            this.tbEchoOutput.Multiline = true;
+            this.tbEchoOutput.Name = "tbEchoOutput";
+            this.tbEchoOutput.Size = new System.Drawing.Size(113, 110);
+            this.tbEchoOutput.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Async";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnEchoAsync_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Input:";
+            // 
+            // btnEchoSync
+            // 
+            this.btnEchoSync.Location = new System.Drawing.Point(59, 45);
+            this.btnEchoSync.Name = "btnEchoSync";
+            this.btnEchoSync.Size = new System.Drawing.Size(50, 23);
+            this.btnEchoSync.TabIndex = 1;
+            this.btnEchoSync.Text = "Sync";
+            this.btnEchoSync.UseVisualStyleBackColor = true;
+            this.btnEchoSync.Click += new System.EventHandler(this.btnEchoSync_Click);
+            // 
+            // tbEchoInput
+            // 
+            this.tbEchoInput.Location = new System.Drawing.Point(59, 19);
+            this.tbEchoInput.Name = "tbEchoInput";
+            this.tbEchoInput.Size = new System.Drawing.Size(113, 20);
+            this.tbEchoInput.TabIndex = 0;
+            this.tbEchoInput.Text = "reply after me";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.label11);
+            this.groupBox5.Controls.Add(this.tbVoivodeship);
+            this.groupBox5.Controls.Add(this.label10);
+            this.groupBox5.Controls.Add(this.tbOrder);
+            this.groupBox5.Controls.Add(this.label8);
+            this.groupBox5.Controls.Add(this.tbOrderOutput);
+            this.groupBox5.Controls.Add(this.btnOrderAsync);
+            this.groupBox5.Controls.Add(this.label9);
+            this.groupBox5.Controls.Add(this.btnOrderSync);
+            this.groupBox5.Controls.Add(this.tbOrderer);
+            this.groupBox5.Location = new System.Drawing.Point(398, 12);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(365, 245);
+            this.groupBox5.TabIndex = 7;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Public orders";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 77);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 13);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Output:";
+            // 
+            // tbOrderOutput
+            // 
+            this.tbOrderOutput.Location = new System.Drawing.Point(59, 74);
+            this.tbOrderOutput.Multiline = true;
+            this.tbOrderOutput.Name = "tbOrderOutput";
+            this.tbOrderOutput.Size = new System.Drawing.Size(288, 156);
+            this.tbOrderOutput.TabIndex = 3;
+            // 
+            // btnOrderAsync
+            // 
+            this.btnOrderAsync.Location = new System.Drawing.Point(297, 43);
+            this.btnOrderAsync.Name = "btnOrderAsync";
+            this.btnOrderAsync.Size = new System.Drawing.Size(50, 23);
+            this.btnOrderAsync.TabIndex = 2;
+            this.btnOrderAsync.Text = "Async";
+            this.btnOrderAsync.UseVisualStyleBackColor = true;
+            this.btnOrderAsync.Click += new System.EventHandler(this.btnOrderAsync_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 22);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Orderer:";
+            // 
+            // btnOrderSync
+            // 
+            this.btnOrderSync.Location = new System.Drawing.Point(234, 43);
+            this.btnOrderSync.Name = "btnOrderSync";
+            this.btnOrderSync.Size = new System.Drawing.Size(50, 23);
+            this.btnOrderSync.TabIndex = 1;
+            this.btnOrderSync.Text = "Sync";
+            this.btnOrderSync.UseVisualStyleBackColor = true;
+            this.btnOrderSync.Click += new System.EventHandler(this.btnOrderSync_Click);
+            // 
+            // tbOrderer
+            // 
+            this.tbOrderer.Location = new System.Drawing.Point(59, 19);
+            this.tbOrderer.Name = "tbOrderer";
+            this.tbOrderer.Size = new System.Drawing.Size(113, 20);
+            this.tbOrderer.TabIndex = 0;
+            this.tbOrderer.Text = "99";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(181, 22);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 13);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Order:";
+            // 
+            // tbOrder
+            // 
+            this.tbOrder.Location = new System.Drawing.Point(234, 19);
+            this.tbOrder.Name = "tbOrder";
+            this.tbOrder.Size = new System.Drawing.Size(113, 20);
+            this.tbOrder.TabIndex = 5;
+            this.tbOrder.Text = "2";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(6, 48);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(52, 13);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "Voivode.:";
+            // 
+            // tbVoivodeship
+            // 
+            this.tbVoivodeship.Location = new System.Drawing.Point(59, 45);
+            this.tbVoivodeship.Name = "tbVoivodeship";
+            this.tbVoivodeship.Size = new System.Drawing.Size(113, 20);
+            this.tbVoivodeship.TabIndex = 7;
+            this.tbVoivodeship.Text = "Zachodniopomorskie";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
@@ -196,6 +450,12 @@
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -216,6 +476,29 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnIpgeoSync;
         private System.Windows.Forms.TextBox tbIpGeoInput;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbTimeOutput;
+        private System.Windows.Forms.Button btnTimeAsync;
+        private System.Windows.Forms.Button btnTimeSync;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbEchoOutput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnEchoSync;
+        private System.Windows.Forms.TextBox tbEchoInput;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox tbVoivodeship;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tbOrder;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbOrderOutput;
+        private System.Windows.Forms.Button btnOrderAsync;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnOrderSync;
+        private System.Windows.Forms.TextBox tbOrderer;
     }
 }
 
--- Comp02_NetAssembly/Form1.cs
+++ Comp02_NetAssembly/Form1.cs
@@ -18,24 +18,26 @@ namespace Comp02_NetAssembly
         }
 
         #region StockQuote
-        private Task<decimal> QuoteTask;
-        private void btnStockQuoteSync_Click(object sender, EventArgs e)
+        string StockQuoteSync(string Stock)
         {
             var ServiceClient = new ServiceReferenceQuotes.DelayedStockQuoteSoapClient("DelayedStockQuoteSoap");
-            tbStockOutput.Text = ServiceClient.GetQuickQuote(tbStockInput.Text, "0").ToString();
+            return ServiceClient.GetQuickQuote(Stock, "0").ToString();
         }
-
-        private void btnStockQuoteAsync_Click(object sender, EventArgs e)
+        Task<decimal> StockQuoteAsync(string Stock)
         {
             var ServiceClient = new ServiceReferenceQuotes.DelayedStockQuoteSoapClient("DelayedStockQuoteSoap");
-            QuoteTask = ServiceClient.GetQuickQuoteAsync(tbStockInput.Text, "0");
-            tbStockOutput.Text = QuoteTask.Result.ToString();
+            return ServiceClient.GetQuickQuoteAsync(Stock, "0");
+        }
+        string EndStockQuote(Task<decimal> StockQuote)
+        {
+            return StockQuote.Result.ToString();
         }
 
+        private void btnStockQuoteSync_Click(object sender, EventArgs e)  => tbStockOutput.Text = StockQuoteSync(tbStockInput.Text);
+        private void btnStockQuoteAsync_Click(object sender, EventArgs e) => tbStockOutput.Text = EndStockQuote(StockQuoteAsync(tbStockInput.Text));
         #endregion
 
-        private Task<ServiceResolve.IPInformation> IpgeoTask;
-
+        #region IpGeo
         private string FormatIpgeo(string Ip, ServiceResolve.IPInformation IpInfo)
         {
             return string.Format("{0}\r\n{1}\r\n{2}\r\n",
@@ -44,20 +46,108 @@ namespace Comp02_NetAssembly
                 Ip
                 );
         }
-
-        private void btnIpgeoSync_Click(object sender, EventArgs e)
+        string IpGeoSync(string IP)
         {
             var ServiceClient = new ServiceResolve.P2GeoSoapClient("IP2GeoSoap");
-            string ip = tbIpGeoInput.Text;
-            tbIpGeoOutput.Text = FormatIpgeo(ip, ServiceClient.ResolveIP(ip, "0"));
+            return FormatIpgeo(IP, ServiceClient.ResolveIP(IP, "0"));
         }
-
-        private void btnIpgeoAsync_Click(object sender, EventArgs e)
+        Task<ServiceResolve.IPInformation> IpGeoAsync(string IP)
         {
             var ServiceClient = new ServiceResolve.P2GeoSoapClient("IP2GeoSoap");
-            string ip = tbIpGeoInput.Text;
-            IpgeoTask = ServiceClient.ResolveIPAsync(ip, "0");
-            tbIpGeoOutput.Text = FormatIpgeo(ip, IpgeoTask.Result);
+            return ServiceClient.ResolveIPAsync(IP, "0");
         }
+        string EndIpGeo(Task<ServiceResolve.IPInformation> IpGeo)
+        {
+            return FormatIpgeo("", IpGeo.Result);
+        }
+
+        private void btnIpgeoSync_Click(object sender, EventArgs e) => tbIpGeoOutput.Text = IpGeoSync(tbIpGeoInput.Text);
+        private void btnIpgeoAsync_Click(object sender, EventArgs e) => tbIpGeoOutput.Text = EndIpGeo(IpGeoAsync(tbIpGeoInput.Text));
+        #endregion
+
+        #region Time
+        string TimeSync()
+        {
+            var ServiceClient = new ServiceEcho.ServiceSoapClient("ServiceSoap1");
+            return ServiceClient.GetTime();
+        }
+        Task<string> TimeAsync()
+        {
+            var ServiceClient = new ServiceEcho.ServiceSoapClient("ServiceSoap1");
+            return ServiceClient.GetTimeAsync();
+        }
+        string EndTimeAsync(Task<string> Time)
+        {
+            return Time.Result;
+        }
+
+        private void btnTimeSync_Click(object sender, EventArgs e)  => tbTimeOutput.Text = TimeSync();
+        private void btnTimeAsync_Click(object sender, EventArgs e) => tbTimeOutput.Text = EndTimeAsync(TimeAsync());
+        #endregion
+
+        #region Echo
+        string EchoSync(string Msg)
+        {
+            var ServiceClient = new ServiceEcho.ServiceSoapClient("ServiceSoap1");
+            return ServiceClient.Echo(Msg);
+        }
+        Task<string> EchoAsync(string Msg)
+        {
+            var ServiceClient = new ServiceEcho.ServiceSoapClient("ServiceSoap1");
+            return ServiceClient.EchoAsync(Msg);
+        }
+        string EndEchoAsync(Task<string> Echo)
+        {
+            return Echo.Result;
+        }
+
+        private void btnEchoSync_Click(object sender, EventArgs e)  => tbEchoOutput.Text =              EchoSync (tbEchoInput.Text);
+        private void btnEchoAsync_Click(object sender, EventArgs e) => tbEchoOutput.Text = EndEchoAsync(EchoAsync(tbEchoInput.Text));
+        #endregion
+
+        #region Public orders
+        string OrderString(DataSet OrderResult)
+        {
+            var Row0 = OrderResult.Tables[0].Rows[0];
+
+            return string.Format(
+                "Received {0} results. Showing result id 0 highlights:\r\n\r\nZamawiajacy Nazwa: {1}\r\n" +
+                "Zamawiajacy REGON: {2}\r\nNazwa projektu: {3}\r\nFinansowanie z Unii: {4}\r\nData publikacji: {5}\r\n",
+                 OrderResult.Tables[0].Rows.Count,
+                 Row0["zamawiajacy_nazwa"],
+                 Row0["regon"],
+                 Row0["nazwa_projektu_programu"],
+                 Row0["czy_finansowane_z_unii"],
+                 Row0["data_publikacji"]
+                );
+        }
+        string OrderSync(Int32 OrdererId, Int32 OrderId, string Voivodeship)
+        {
+            var ServiceClient = new ServicePublicData.BZP_PublicWebServiceSoapClient("BZP_PublicWebServiceSoap");
+            return OrderString(ServiceClient.ogloszeniaZP400KryteriaWyszukiwaniaDataSet(
+                OrdererId, OrderId, 99,
+                "", "", "", "", "", 99,
+                "", "",
+                Voivodeship, -1, -1));
+        }
+        Task<DataSet> OrderAsync(Int32 OrdererId, Int32 OrderId, string Voivodeship)
+        {
+            var ServiceClient = new ServicePublicData.BZP_PublicWebServiceSoapClient("BZP_PublicWebServiceSoap");
+            return ServiceClient.ogloszeniaZP400KryteriaWyszukiwaniaDataSetAsync(
+                OrdererId, OrderId, 99,
+                "", "", "", "", "", 99,
+                "", "",
+                Voivodeship, -1, -1);
+        }
+        string EndOrderAsync(Task<DataSet> OrderDataset)
+        {
+            return OrderString(OrderDataset.Result);
+        }
+
+        private void btnOrderSync_Click(object sender, EventArgs e) 
+            => tbOrderOutput.Text = OrderSync               (Convert.ToInt32(tbOrderer.Text), Convert.ToInt32(tbOrder.Text), tbVoivodeship.Text);
+        private void btnOrderAsync_Click(object sender, EventArgs e) 
+            => tbOrderOutput.Text = EndOrderAsync(OrderAsync(Convert.ToInt32(tbOrderer.Text), Convert.ToInt32(tbOrder.Text), tbVoivodeship.Text));
+        #endregion
     }
 }
