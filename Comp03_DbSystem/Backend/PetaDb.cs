dcd6e7b402c4feea07f1180ec856696223de1e0c
Krzysztof Niedźwiecki <k.niedzwiecki@hotmail.com>
2019-03-30 17:03:50 +0100

    added PetaPoco library and created db classes in Backend assembly

---
+++ Comp03_DbSystem/Backend/PetaDb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Backend
+{
+    [PetaPoco.TableName("Users")]
+    [PetaPoco.PrimaryKey("id")]
+    public partial class User
+    {
+        [PetaPoco.Column("id")] public Int64 Id { get; set; }
+        [PetaPoco.Column("username")] public String Username{ get; set; }
+    }
+
+    [PetaPoco.TableName("Tasklists")]
+    [PetaPoco.PrimaryKey("id")]
+    public partial class Tasklist
+    {
+        [PetaPoco.Column("id")] public Int64 Id { get; set; }
+        [PetaPoco.Column("name")] public String Name { get; set; }
+        [PetaPoco.Column("deadline")] public Int32 Deadline { get; set; }
+    }
+
+    [PetaPoco.TableName("Tasks")]
+    [PetaPoco.PrimaryKey("id")]
+    public partial class Task
+    {
+        [PetaPoco.Column("id")] public Int64 Id { get; set; }
+        [PetaPoco.Column("name")] public String Name { get; set; }
+        [PetaPoco.Column("descripion")] public String Description { get; set; }
+        [PetaPoco.Column("deadline")] public Int32 Deadline { get; set; }
+        [PetaPoco.Column("status")] public Int32 Status { get; set; }
+        [PetaPoco.Column("owner_tasklist")] public Int64 TasklistId { get; set; }
+        [PetaPoco.Column("assignee_user")] public Int64 UserId { get; set; }
+    }
+}
