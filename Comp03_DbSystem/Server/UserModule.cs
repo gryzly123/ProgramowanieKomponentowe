2893d702c003b8d77a7a0cd865a5a10237eca81d
Krzysztof Niedźwiecki <k.niedzwiecki@hotmail.com>
2019-03-31 00:08:41 +0100

    finished User API

--- Comp03_DbSystem/Server/UserModule.cs
+++ Comp03_DbSystem/Server/UserModule.cs
@@ -1,14 +1,24 @@
-﻿using System.Collections.Generic;
-using Nancy;
+﻿using Nancy;
+using Nancy.Extensions;
+using System;
 
 namespace Server
 {
+    public class Json
+    {
+        public static dynamic Parse(string Json) { return Newtonsoft.Json.JsonConvert.DeserializeObject(Json); }
+        public static dynamic Parse(Request Req) { return Newtonsoft.Json.JsonConvert.DeserializeObject(Req.Body.AsString()); }
+    }
+
     public class UserModule : NancyModule
     {
         public UserModule() : base("/User")
         {
             Get["/{id}"] = parameter => { return GetById(parameter.id); };
-            Get["/all"] = parameter =>  { return GetAll(); };
+            Delete["/{id}"] = parameter => { return RemoveUser(parameter.id); };
+            Put["/{id}"] = parameter => { return UpdateUser(parameter.id, Json.Parse(Request)); };
+            Get["/all"] = parameter => { return GetAll(); };
+            Post["/add"] = parameter => { return AddUser(Json.Parse(Request)); };
         }
 
         object GetById(int id)
@@ -25,20 +35,44 @@ namespace Server
         object GetAll()
         {
             PetaPoco.Database db = Backend.Sysdata.Get();
+
             return db.Query<Backend.User>(Backend.User.sqlGetAll());
+        }
 
-            List<Dictionary<string, string>> result = new List<Dictionary<string, string>>();
+        object AddUser(dynamic parameters)
+        {
+            PetaPoco.Database db = Backend.Sysdata.Get();
 
-            foreach (Backend.User u in db.Query<Backend.User>(Backend.User.sqlGetAll()))
+            object retval = db.Insert("Users", "id", new { username = parameters["username"] });
+            if(retval is long) return new { ok = retval };
+            return new { error = "internal_error" };
+        }
+
+        object RemoveUser(int id)
+        {
+            PetaPoco.Database db = Backend.Sysdata.Get();
+
+            int retval = db.Delete("Users", "id", new { id = id });
+            return (retval == 1) ? null : new { error = "user_doesnt_exist" };
+        }
+
+        object UpdateUser(int id, dynamic parameters)
+        {
+            PetaPoco.Database db = Backend.Sysdata.Get();
+
+            Backend.User u = null;
+            try
             {
-                Dictionary<string, string> userdict = new Dictionary<string, string>();
-                userdict.Add("id", u.Id.ToString());
-                userdict.Add("username", u.Username);
-                result.Add(userdict);
+                u = (Backend.User)GetById(id);
+                if (parameters["username"] != null) u.Username = parameters["username"].ToString();
+            }
+            catch(Exception e)
+            {
+                return new { error = "user_doesnt_exist" };
             }
 
-            if(result.Count == 0) return new { error = "no_users" };
-            return result;
+            int retval = db.Update("Users", "id", u, u.Id);
+            return (retval == 1) ? null : new { error = "update_failed" };
         }
     }
 }
